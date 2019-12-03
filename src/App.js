02413ada282ec780206d105e17ab28d8346b7098
Marcos Iglesias <markkos89@gmail.com>
2019-12-03 20:06:00 -0300

    calculateWordCount()

--- src/App.js
+++ src/App.js
@@ -9,12 +9,18 @@ const App = () => {
     setText(value)
   }
 
+  const calculateWordCount = (text) => {
+    const wordsArr = text.trim().split(" ")
+    const filteredWords = wordsArr.filter(word => word !== "")
+    return filteredWords.length
+  }
+
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={text} />
       <h4>Time remaining: ???</h4>
-      <button>Start</button>
+      <button onClick={() => calculateWordCount(text)}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   )
