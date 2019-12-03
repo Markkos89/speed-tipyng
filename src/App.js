038311f50d41e34ef47ff96ee8fc211be4164cb6
Marcos Iglesias <markkos89@gmail.com>
2019-12-03 19:21:55 -0300

    added state. added font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import './App.css'
 
 const App = () => {
+  const [text, setText] = useState("")
+
+  const handleChange = (e) => {
+    const { value } = e.target
+    setText(value)
+  }
+
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea />
+      <textarea onChange={handleChange} value={text} />
       <h4>Time remaining: ???</h4>
       <button>Start</button>
       <h1>Word count: ???</h1>
