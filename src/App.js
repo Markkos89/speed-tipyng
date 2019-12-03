026e981b332be04369c5ee17ae87305aa5f53aaa
Marcos Iglesias <markkos89@gmail.com>
2019-12-03 19:18:07 -0300

    typo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react"
 import './App.css'
-/**
- * Challenge: Using hooks, track the state of the text in the textarea on every keystroke
- * To verify it's working, you could just console.log the state on every change
- */
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea />
-      <h4>Time reminaing: ???</h4>
+      <h4>Time remaining: ???</h4>
       <button>Start</button>
       <h1>Word count: ???</h1>
     </div>
