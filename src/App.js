e03e8e982c4fb19ab76caab5a0c5883c526aa44f
Marcos Iglesias <markkos89@gmail.com>
2019-12-03 23:29:59 -0300

    isTimeRunning?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import './App.css'
 
 const App = () => {
   const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(5)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
 
   const handleChange = (e) => {
     const { value } = e.target
@@ -15,12 +17,22 @@ const App = () => {
     return filteredWords.length
   }
 
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1)
+      }, 1000)
+    } else if (timeRemaining === 0) {
+      setIsTimeRunning(false)
+    }
+  }, [timeRemaining, isTimeRunning])
+
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={text} />
-      <h4>Time remaining: ???</h4>
-      <button onClick={() => calculateWordCount(text)}>Start</button>
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={() => setIsTimeRunning(true)}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   )
